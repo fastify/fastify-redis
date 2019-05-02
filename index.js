edfb960598c0ed3bc4fb2e02ab46307fd03b7cc7
darkgl0w <31093081+darkgl0w@users.noreply.github.com>
2019-05-02 16:12:30 +0200

    Allow multiple redis instances registering (#29)

--- index.js
+++ index.js
@@ -4,18 +4,52 @@ const fp = require('fastify-plugin')
 const Redis = require('ioredis')
 
 function fastifyRedis (fastify, options, next) {
-  var client = options.client || null
+  const namespace = options.namespace
+  delete options.namespace
 
-  if (!client) {
-    try {
-      client = new Redis(options)
-    } catch (err) {
-      return next(err)
+  let client = options.client || null
+
+  if (namespace) {
+    if (!fastify.redis) {
+      fastify.decorate('redis', {})
     }
-    fastify.addHook('onClose', close)
-  }
 
-  fastify.decorate('redis', client)
+    if (fastify.redis[namespace]) {
+      return next(new Error(`Redis '${namespace}' instance namespace has already been registered`))
+    }
+
+    const closeNamedInstance = (fastify, done) => {
+      fastify.redis[namespace].quit(done)
+    }
+
+    if (!client) {
+      try {
+        client = new Redis(options)
+      } catch (err) {
+        return next(err)
+      }
+
+      fastify.addHook('onClose', closeNamedInstance)
+    }
+
+    fastify.redis[namespace] = client
+  } else {
+    if (fastify.redis) {
+      next(new Error('fastify-redis has already been registered'))
+    } else {
+      if (!client) {
+        try {
+          client = new Redis(options)
+        } catch (err) {
+          return next(err)
+        }
+
+        fastify.addHook('onClose', close)
+      }
+
+      fastify.decorate('redis', client)
+    }
+  }
 
   next()
 }
