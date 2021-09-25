325f01777a9c0b65146046fe37381cf146649818
darkgl0w <31093081+darkgl0w@users.noreply.github.com>
2021-09-25 13:04:56 +0200

    feat (types) !: add namespaced redis instances + explicit naming

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,27 @@
 import { FastifyPluginCallback } from 'fastify';
 import { Redis, RedisOptions } from 'ioredis';
 
+export interface FastifyRedisNamespacedInstance {
+  [namespace: string]: Redis;
+}
+
+export type FastifyRedis = FastifyRedisNamespacedInstance & Redis;
+
 declare module 'fastify' {
   interface FastifyInstance {
-    redis: Redis;
+    redis: FastifyRedis;
   }
 }
 
-export type FastifyRedisPlugin = RedisOptions &
+export type FastifyRedisPluginOptions = (RedisOptions &
 {
   url?: string;
   namespace?: string;
-} |
-{
+}) | {
   client: Redis;
   namespace?: string;
   closeClient?: boolean;
 }
 
-declare const fastifyRedis: FastifyPluginCallback<FastifyRedisPlugin>;
-
+declare const fastifyRedis: FastifyPluginCallback<FastifyRedisPluginOptions>;
 export default fastifyRedis;
