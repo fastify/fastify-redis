e1407028e8ea007a9a711ff04014a82e5d0a960e
Antonio Tripodi <Tony133@users.noreply.github.com>
2026-04-26 11:46:26 +0200

    refactor(types): migrate from tsd to tstyche (#249)
    * refactor(types): migrate from tsd to tstyche
    
    * chore: update package.json
    
    * chore: update index.tst.ts

--- types/index.tst.ts
+++ types/index.tst.ts
@@ -1,7 +1,7 @@
 import Fastify, { FastifyInstance } from 'fastify'
 import IORedis, { Redis } from 'ioredis'
-import { expectAssignable, expectDeprecated, expectError, expectType } from 'tsd'
-import fastifyRedis, { FastifyRedis, FastifyRedisPlugin, FastifyRedisNamespacedInstance, FastifyRedisPluginOptions } from '..'
+import { expect } from 'tstyche'
+import fastifyRedis, { FastifyRedis, FastifyRedisNamespacedInstance, FastifyRedisPluginOptions } from '.'
 
 const app: FastifyInstance = Fastify()
 const redis: Redis = new IORedis({ host: 'localhost', port: 6379 })
@@ -20,23 +20,21 @@ app.register(fastifyRedis, {
   url: 'redis://127.0.0.1:6379'
 })
 
-expectAssignable<FastifyRedisPluginOptions>({
+expect<FastifyRedisPluginOptions>().type.toBeAssignableFrom({
   client: redisCluster
 })
 
-expectError(app.register(fastifyRedis, {
+expect(app.register).type.not.toBeCallableWith(fastifyRedis, {
   namespace: 'three',
   unknownOption: 'this should trigger a typescript error'
-}))
+})
 
 // Plugin property available
 app.after(() => {
-  expectAssignable<Redis>(app.redis)
-  expectType<FastifyRedis>(app.redis)
+  expect(app.redis).type.toBeAssignableTo<Redis>()
+  expect(app.redis).type.toBe<FastifyRedis>()
 
-  expectAssignable<FastifyRedisNamespacedInstance>(app.redis)
-  expectType<Redis>(app.redis.one)
-  expectType<Redis>(app.redis.two)
+  expect(app.redis).type.toBeAssignableTo<FastifyRedisNamespacedInstance>()
+  expect(app.redis.one).type.toBe<Redis | undefined>()
+  expect(app.redis.two).type.toBe<Redis | undefined>()
 })
-
-expectDeprecated({} as FastifyRedisPlugin)
